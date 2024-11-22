329c4f9d8416e8a6bb41582995c071d6f1addf2a
rennwebsite <167429751+rennwebsite@users.noreply.github.com>
2024-11-23 04:26:44 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,8 +1,13 @@
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("../coffe.json");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const router = jsonServer.router(path.join(__dirname, "../coffe.json")); // Lokasi file JSON
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(router);
-module.exports = server;
+app.use(middlewares); // Middleware standar
+app.use(express.json()); // Untuk membaca body JSON
+app.use(router); // Semua route CRUD dikelola JSON Server
+
+module.exports = app;
